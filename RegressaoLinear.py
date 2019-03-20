738e201abf661967eb950beb66f67715de7cf8fa
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2019-03-20 12:09:11 -0300

    Adicao da funcao da gradiente descendente

--- RegressaoLinear.py
+++ RegressaoLinear.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import FuncoesAuxiliares as f
 import numpy as np
+
 class RegressaoLinear:
-    def __init__(self, teta0,teta1,dadosRegressao):
+    def __init__(self, teta0, teta1, dadosRegressao, alpha, qtVezes):
         self.t0 = teta0
         self.t1 = teta1
         self.dados = dadosRegressao
+        self.alpha = alpha
+        self.qtVezes = qtVezes
 
     def retornaPredicao(self, x):
         return self.t0+(self.t1*x)
@@ -15,7 +18,17 @@ class RegressaoLinear:
         print 
         for index, row in self.dados.iterrows():
             soma += ((self.retornaPredicao(np.float(row[column1]))-np.float(row[column2]))**2)
-        print (soma)
         return soma/(2*(len(self.dados)))
 
-
+    def calculaGradienteDescendente(self, column1, column2):
+        tempT0 = self.t0
+        tempT1 = self.t1
+        i = 0
+        while (i < self.qtVezes):
+            for index, row in self.dados.iterrows():
+                tempT0 += (-(1.0/len(self.dados))*(np.float(row[column2])-self.retornaPredicao(np.float(row[column1]))))
+                tempT1 += (-(1.0/len(self.dados))*(np.float(row[column2])-self.retornaPredicao(np.float(row[column1])))*np.float(row[column1]))
+            self.t0 -= tempT0*self.alpha
+            self.t1 -= tempT1*self.alpha
+            i += 1
+        return [self.t0, self.t1]
--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import pandas as pd
 import FuncoesAuxiliares as f
 import pandas as pd
 import RegressaoLinear as rl
+import numpy as np
+import matplotlib.pyplot as plt
+
 '''
 #Dados do primeiro dataset sobre peso do corpo e peso do cerebro
 '''
@@ -12,6 +15,8 @@ dados_df = pd.DataFrame(resultado, columns=['index','brainw','bodyw'])
 
 dados_df = dados_df.drop('index',axis=1)
 
-rl = rl.RegressaoLinear(2, 0, dados_df)
-print (rl.retornaFuncaoCusto('brainw','bodyw'))
+rl = rl.RegressaoLinear(2, 0, dados_df, 0.1, 60) 
+#print (rl.retornaFuncaoCusto('brainw','bodyw'))
+print (rl.calculaGradienteDescendente('brainw','bodyw'))
 
+plt.scatter(dados_df['brainw'], dados_df['bodyw']) 
