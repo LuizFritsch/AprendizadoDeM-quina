1cf151c5e9f6c8ccf214df65117b9f18b058abbf
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2019-03-21 08:56:08 -0300

    Modificações na Regressao Linear, funcionando 99%

--- Reglin.py
+++ Reglin.py
@@ -1,21 +1,31 @@
-from arquivos import *
+#from FuncoesAuxiliares import *
+#from FuncoesEstatisticas import *
 from math import *
 from numpy import *
 import pandas as np
+import arquivos as a
 
+#
 def hipotese(x, th0, th1):
 	return th0 + th1 * x
 
-dataset=readtable("x06.txt")
+#Le os dados e armazena na matriz
+dataset=a.readtable("x03.txt")
 
-alfa=0.0001
+#Taxa de aprendizado, quanto menor o alfa, 
+alpha=0.0001
 
+#Funcao pra achar a melhor reta
 def treinar(x,y,alpha):
 	qtdAmostras=len(x)
+	#Nao entendi qual deve ser exatamente o theta 0 e theta 1 iniciais
 	theta0=0.0
 	theta1=1.0
+	#taxa de erro
 	erro=666
-	while(erro>0.001):
+	#Condicao de parada: so para quando a taxa de erro for maior que 0.00001
+	while(erro>0.00001):
+
 		dtheta0=0.0
 		dtheta1=0.0
 		for i in range(0,qtdAmostras):
@@ -25,12 +35,16 @@ def treinar(x,y,alpha):
 		tempt1=theta1
 		theta0=theta0-alpha*dtheta0/qtdAmostras
 		theta1=theta1-alpha*dtheta1/qtdAmostras
+		print('---------------')
+		print('-',erro,'-')
+		print('---------------')
 		erro=math.sqrt((theta0-tempt0)**2+(theta1-tempt1)**2)
-	plot (x, y, "bo")
-	plot (x, [hipotese(i, theta0, theta1) for i in x], "r", linewidth=3, label='modelo')
+	plot (x,y,"bo")
+	#ynovo=[hipotese(i,theta0,theta1) for i in x]
+	#plot (x,ynovo,"r",linewidth=3,label='modelo')
 	show()
 	return[theta0, theta1]
 
-x = asarray(column(dataset,2))
-y = asarray(column(dataset,3))
-print treinar(x, y, alfa)
+x = asarray(a.column(dataset,2))
+y = asarray(a.column(dataset,3))
+print (treinar(x, y, alpha))
